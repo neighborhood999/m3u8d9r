ab6486a170d7104874603cc29b9d79d252608ff5
Peng Jie <bivinity.pengzjie@gmail.com>
2018-12-11 12:31:44 +0800

    fix(script): add default filename

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # m3u8d9r
 
+A simple shell script to download mp4 video from the m3u8 address by [FFmpeg](https://www.ffmpeg.org/).
+
 ## LICENSE
 
 MIT
--- script.sh
+++ script.sh
@@ -4,15 +4,16 @@ set -e
 
 if [[ -z $1 ]]; then
   echo -e "Usage: script.sh [m3u8_address] [filename]\n";
-  echo -e "\x1b[91;1mError: m3u8 source is required.\x1b[0m";
   exit;
 fi
 
-if [[ -z $2 ]]; then
-  echo -e "\x1b[91;1mError: filename is required.\x1b[0m";
-  exit;
+file=$(basename "$1");
+filename=${file%.*};
+
+if [[ ! -z $2 ]]; then
+  filename="$2"
 fi
 
-ffmpeg -i "$1" -c copy -bsf:a aac_adtstoasc "$2.mp4";
+ffmpeg -i "$1" -c copy -bsf:a aac_adtstoasc "$filename.mp4";
 
-echo -e "Done\n";
+echo -e "\033[0;32mDownload is completed.\033[0m";
